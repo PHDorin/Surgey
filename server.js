1a2d1f2477a66a4af25910f51ba0f07dfc933b0d
PHDorin <hanshirosan@gmail.com>
2017-01-05 11:42:27 -0800

    Attempt to complete deployment

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const port = process.env.PORT || 1337;
+const ip = process.env.IP || '127.0.0.1';
+app = express();
+app.use(morgan('dev'))
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+})
+app.use(express.static('./frontend/dist'))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+// Routes
+// require('./routes/routes')(app)
+
+app.listen(port, function() {
+  console.log('The magic is on ' + ip + ':' + port);
+});
